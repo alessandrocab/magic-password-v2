07b0dd4e9a38e9fa00d368e5dd9127a40f137ba1
alessandrocab <alessandrocabriolu@hotmail.com>
2025-05-18 14:49:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function draw() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "#0F0";
+  ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
   ctx.font = `${fontSize}px monospace`;
 
   for (let i = 0; i < drops.length; i++) {
